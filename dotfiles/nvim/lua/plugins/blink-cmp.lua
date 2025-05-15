15b5254ef3368cd8c6009313bb8f7af3f6ed73a7
Danny Breyfogle <27653146+dbreyfogle@users.noreply.github.com>
2025-05-15 03:08:32 -0700

    feat: update keymaps

--- dotfiles/nvim/lua/plugins/blink-cmp.lua
+++ dotfiles/nvim/lua/plugins/blink-cmp.lua
@@ -2,54 +2,52 @@ return {
   "saghen/blink.cmp",
   version = "1.*",
   dependencies = { "rafamadriz/friendly-snippets" },
-  init = function()
-    -- Hide copilot suggestions when menu is open
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "BlinkCmpMenuOpen",
-      callback = function()
-        vim.b.copilot_suggestion_hidden = true
+  config = function()
+    require("blink.cmp").setup({
+      -- Disable completions for certain filetypes
+      enabled = function()
+        return not vim.tbl_contains({
+          "gitcommit",
+          "gitrebase",
+          "markdown",
+          "oil",
+          "txt",
+        }, vim.bo.filetype)
       end,
+
+      cmdline = {
+        completion = { menu = { auto_show = true } },
+        keymap = { preset = "inherit" },
+      },
+
+      completion = {
+        accept = { auto_brackets = { enabled = true } },
+        documentation = { auto_show = true },
+        ghost_text = { enabled = true },
+      },
+
+      keymap = {
+        preset = "default",
+        ["<Tab>"] = { "accept", "fallback" },
+        ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+        ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-f>"] = { "snippet_forward" },
+        ["<C-b>"] = { "snippet_backward" },
+      },
+
+      signature = { enabled = true },
     })
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "BlinkCmpMenuClose",
-      callback = function()
-        vim.b.copilot_suggestion_hidden = false
-      end,
-    })
+
+    -- Additional snippet keymaps
+    local cmp = require("blink.cmp")
+    vim.keymap.set({ "n" }, "<C-f>", function()
+      cmp.snippet_forward()
+    end)
+    vim.keymap.set({ "n" }, "<C-b>", function()
+      cmp.snippet_backward()
+    end)
+    vim.keymap.set({ "i", "n", "s" }, "<C-h>", function()
+      vim.snippet.stop()
+    end)
   end,
-  opts = {
-    -- Disable completions for certain filetypes
-    enabled = function()
-      return not vim.tbl_contains({
-        "gitcommit",
-        "gitrebase",
-        "markdown",
-        "oil",
-        "txt",
-      }, vim.bo.filetype)
-    end,
-
-    cmdline = {
-      completion = { menu = { auto_show = true } },
-      keymap = { preset = "inherit" },
-    },
-
-    completion = {
-      accept = { auto_brackets = { enabled = true } },
-      documentation = { auto_show = true },
-      ghost_text = { enabled = true },
-    },
-
-    keymap = {
-      preset = "default",
-      ["<Tab>"] = { "accept", "fallback" },
-      ["<C-d>"] = { "scroll_documentation_down", "fallback" },
-      ["<C-u>"] = { "scroll_documentation_up", "fallback" },
-      ["<C-f>"] = { "snippet_forward", "fallback" },
-      ["<S-Tab>"] = { "snippet_backward", "fallback" },
-      ["<M-\\>"] = { "hide", "fallback" }, -- copilot suggestion
-    },
-
-    signature = { enabled = true },
-  },
 }
--- dotfiles/nvim/lua/plugins/copilot.lua
+++ dotfiles/nvim/lua/plugins/copilot.lua
@@ -1,23 +1,37 @@
 return {
   "zbirenbaum/copilot.lua",
+  dependencies = { "saghen/blink.cmp" },
   event = "InsertEnter",
   cmd = "Copilot",
-  keys = {
-    {
-      "<M-\\>",
-      function()
-        require("copilot.suggestion").next()
-      end,
-      mode = "i",
-    },
-  },
-  opts = {
-    suggestion = {
-      trigger_on_accept = false,
-      keymap = {
-        accept = "<Tab>",
-        dismiss = "<C-e>",
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+        trigger_on_accept = false,
+        keymap = {
+          accept = "<Tab>",
+          dismiss = "<C-e>",
+        },
       },
-    },
-  },
+    })
+
+    -- Hide completion menu and trigger suggestions
+    vim.keymap.set({ "i" }, "<C-j>", function()
+      require("blink.cmp").hide()
+      require("copilot.suggestion").next()
+    end)
+
+    -- Hide suggestions when completion menu is open
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "BlinkCmpMenuOpen",
+      callback = function()
+        vim.b.copilot_suggestion_hidden = true
+      end,
+    })
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "BlinkCmpMenuClose",
+      callback = function()
+        vim.b.copilot_suggestion_hidden = false
+      end,
+    })
+  end,
 }
