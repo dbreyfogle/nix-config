b394fba8b38415494f2ebc550a8ceca7d22f3999
Danny Breyfogle <27653146+dbreyfogle@users.noreply.github.com>
2025-05-27 12:39:23 -0700

    feat: update python template

--- templates/python/flake.nix
+++ templates/python/flake.nix
@@ -1,77 +1,32 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    pyproject-nix = {
-      url = "github:pyproject-nix/pyproject.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    uv2nix = {
-      url = "github:pyproject-nix/uv2nix";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    pyproject-build-systems = {
-      url = "github:pyproject-nix/build-system-pkgs";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.uv2nix.follows = "uv2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
-    {
-      self,
-      flake-utils,
-      nixpkgs,
-      pyproject-nix,
-      uv2nix,
-      pyproject-build-systems,
-      ...
-    }:
+    { flake-utils, nixpkgs, ... }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        inherit (nixpkgs) lib;
-        workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
-        overlay = workspace.mkPyprojectOverlay { sourcePreference = "wheel"; };
         pkgs = nixpkgs.legacyPackages.${system};
-        python = pkgs.python312;
-        pythonSet =
-          (pkgs.callPackage pyproject-nix.build.packages {
-            inherit python;
-          }).overrideScope
-            (
-              lib.composeManyExtensions [
-                pyproject-build-systems.overlays.default
-                overlay
-              ]
-            );
+        python = pkgs.python312Full;
       in
       {
-        packages.default = pythonSet.mkVirtualEnv "venv" workspace.deps.default;
-
-        apps.default = {
-          type = "app";
-          program = "${self.packages.${system}.default}/bin/python-template";
-        };
-
         devShells.default = pkgs.mkShell {
-          packages = [
-            python
-            pkgs.uv
-          ];
+          packages =
+            [ python ]
+            ++ (with pkgs; [
+              ruff
+              uv
+            ]);
           env =
             {
               UV_PYTHON_DOWNLOADS = "never";
               UV_PYTHON = python.interpreter;
             }
-            // lib.optionalAttrs (pkgs.stdenv.isLinux && builtins.pathExists "/etc/nixos") {
-              LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+            // nixpkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
+              LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
             };
           shellHook = ''
             unset PYTHONPATH
