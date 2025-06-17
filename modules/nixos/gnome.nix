1236ba279e698e95d8227241faf700d0f3714d8b
Danny Breyfogle <27653146+dbreyfogle@users.noreply.github.com>
2025-06-16 21:12:28 -0700

    fix: renamed nix options

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 
 {
+  services.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
   services.xserver = {
     enable = true;
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
     excludePackages = [ pkgs.xterm ];
   };
 
