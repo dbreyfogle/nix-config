31352a61dac4d45490d015b317122b649bafd6af
Danny Breyfogle <27653146+dbreyfogle@users.noreply.github.com>
2025-11-01 10:41:11 -0700

    chore: update lock files

--- pkgs/dbt-fusion/package.nix
+++ pkgs/dbt-fusion/package.nix
@@ -12,30 +12,30 @@ let
   systemToPlatform = {
     "x86_64-linux" = {
       name = "x86_64-unknown-linux-gnu";
-      dbtHash = "sha256-sM0DUfDq2fpenivmErkJa8x9tP889Its8/6nPe58WRU=";
-      lspHash = "sha256-YHWutfw7IzS+b4ArCI/Z7EBRUKDokvTdWFjheCoKS7c=";
+      dbtHash = "sha256-SY0gIgxyVgjDMIEwta5bDIuYk5YSlB72jEarpqyF/V4=";
+      lspHash = "sha256-ozYy71bcVzYZ326QY8Mh0AbgeJwJQVhVu9Zkw9zlWGQ=";
     };
     "aarch64-linux" = {
       name = "aarch64-unknown-linux-gnu";
-      dbtHash = "sha256-JJBGoPCbMJrEm+l/4oGCl3vukpPLmvE0lBIgOGIn5k8=";
-      lspHash = "sha256-/E2VtR4iVodi6Sfme/yj8m1RkDizdN37ohdthHgajYs=";
+      dbtHash = "sha256-EreskLor9hbuIkWqByrcEH4w9IvIdkUdwsl3gvnWZpA=";
+      lspHash = "sha256-DycRAYbd1WVsqv4xSQdniYwun0SZ4uXH6CDXOHlOlq0=";
     };
     "x86_64-darwin" = {
       name = "x86_64-apple-darwin";
-      dbtHash = "sha256-CIPgamL5FsojN0/Jshf7/euyuUIVwI4N1Zo9s2bihDo=";
-      lspHash = "sha256-tCvyDnoo2tvheHVJQ+8rNo5mhcUC39ryIcA3rsI5Nmc=";
+      dbtHash = "sha256-U+eqq1bm0wct6GSWLF7a6KaSf/cTgSZt2l7p8eNaHK8=";
+      lspHash = "sha256-zxXsiWH9GxtNuPQ/CrUuoFdRKZmy7Z2jyOFnU2xy4A0=";
     };
     "aarch64-darwin" = {
       name = "aarch64-apple-darwin";
-      dbtHash = "sha256-6e+1wlfF+kGmnGVuItv6YMugEgYfk8SOcwAoyW4y8To=";
-      lspHash = "sha256-Jt4LYD3yn6lUlFiqVDMqMqwnMTmVzWmdOCnHzBcF0wc=";
+      dbtHash = "sha256-fbURLqkl0gZgFSs99/91iE0idyKH1nJzbHl7XqUsehc=";
+      lspHash = "sha256-WxbSrdGOBcpi8Rca3h7HUi7Iv7gzvpyemR9qr5R1x4c=";
     };
   };
   platform = systemToPlatform.${system} or throwSystem;
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "dbt-fusion";
-  version = "2.0.0-preview.27";
+  version = "2.0.0-preview.57";
 
   srcs = [
     (fetchurl {
