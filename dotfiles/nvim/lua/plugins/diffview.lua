2826f520ed8ed412ce9759ee4a94d41e7c812678
Danny Breyfogle <27653146+dbreyfogle@users.noreply.github.com>
2026-01-02 15:15:02 -0700

    fix: prevent certain LSPs from attaching to diffview buffers

--- dotfiles/nvim/lua/plugins/diffview.lua
+++ dotfiles/nvim/lua/plugins/diffview.lua
@@ -9,6 +9,22 @@ return {
     { "<Leader>gll", "<CMD>DiffviewFileHistory<CR>", desc = "Git: Open commit history" },
     { "<Leader>glL", "<CMD>DiffviewFileHistory %<CR>", desc = "Git: Open commit history for current file" },
   },
+  init = function()
+    -- Prevent certain LSPs from attaching to diffview buffers
+    local override_filetypes = { "terraform" }
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = override_filetypes,
+      callback = function(args)
+        local bufname = vim.api.nvim_buf_get_name(args.buf)
+        if bufname:match("^diffview://") then
+          vim.bo[args.buf].filetype = "diffview_" .. vim.bo[args.buf].filetype
+        end
+      end,
+    })
+    for _, ft in ipairs(override_filetypes) do
+      vim.treesitter.language.register(ft, "diffview_" .. ft)
+    end
+  end,
   config = function()
     local actions = require("diffview.actions")
 
