ad7616126288cadc28afe93768e14bd32d9dd0da
Danny Breyfogle <27653146+dbreyfogle@users.noreply.github.com>
2026-01-27 20:57:21 -0700

    fix: remove duplicate brave desktop item

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,4 +5,13 @@ final: prev: {
   terraform-versions = inputs.nixpkgs-terraform.overlays.default final prev;
   astro-cli = prev.callPackage ../pkgs/astro-cli/package.nix { };
   dbt-fusion = prev.callPackage ../pkgs/dbt-fusion/package.nix { };
+
+  # fix: remove duplicate brave desktop item
+  brave = prev.brave.overrideAttrs (oldAttrs: {
+    postInstall =
+      (oldAttrs.postInstall or "")
+      + prev.lib.optionalString prev.stdenv.hostPlatform.isLinux ''
+        rm -f $out/share/applications/com.brave.Browser.desktop
+      '';
+  });
 }
