f366955f7500655be39b04e567713fe8cd357992
Danny Breyfogle <27653146+dbreyfogle@users.noreply.github.com>
2026-01-30 13:36:01 -0700

    fix: inherit config.allowUnfree in nixpkgs-stable overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,7 +1,10 @@
 { inputs, ... }:
 
 final: prev: {
-  stable = import inputs.nixpkgs-stable { inherit (prev) system config; };
+  stable = import inputs.nixpkgs-stable {
+    inherit (prev) system;
+    config.allowUnfree = prev.config.allowUnfree;
+  };
   terraform-versions = inputs.nixpkgs-terraform.overlays.default final prev;
   astro-cli = prev.callPackage ../pkgs/astro-cli/package.nix { };
   dbt-fusion = prev.callPackage ../pkgs/dbt-fusion/package.nix { };
