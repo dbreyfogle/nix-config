33f1250f781fb2fb48d7cbc5a897b4a3b5f8c6ea
Danny Breyfogle <27653146+dbreyfogle@users.noreply.github.com>
2026-01-31 18:41:18 -0700

    fix: enable swipe gestures for page navigation

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,10 +9,16 @@ final: prev: {
   astro-cli = prev.callPackage ../pkgs/astro-cli/package.nix { };
   dbt-fusion = prev.callPackage ../pkgs/dbt-fusion/package.nix { };
 
-  # fix: remove duplicate brave desktop item
   brave = prev.brave.overrideAttrs (oldAttrs: {
+    preFixup =
+      builtins.replaceStrings
+        # Enable swipe gestures for page navigation
+        [ "--enable-features=" ] [ "--enable-features=TouchpadOverscrollHistoryNavigation," ]
+        (oldAttrs.preFixup or "");
+
     postInstall =
       (oldAttrs.postInstall or "")
+      # Remove duplicate desktop item
       + prev.lib.optionalString prev.stdenv.hostPlatform.isLinux ''
         rm -f $out/share/applications/com.brave.Browser.desktop
       '';
