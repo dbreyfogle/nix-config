c69a9c9e86af625c70897771d4b7b65aca93cce3
Danny Breyfogle <27653146+dbreyfogle@users.noreply.github.com>
2026-02-14 19:03:20 -0700

    chore: update nixpkgs-stable url

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     nix-darwin.url = "github:nix-darwin/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
