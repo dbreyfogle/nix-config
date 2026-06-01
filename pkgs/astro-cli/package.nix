9d80fa34fe3d2a7175328fa241b4617959f9f791
dbreyfogle <27653146+dbreyfogle@users.noreply.github.com>
2026-06-01 16:09:04 +0000

    ci: auto update

--- pkgs/astro-cli/package.nix
+++ pkgs/astro-cli/package.nix
@@ -10,26 +10,26 @@ let
   systemToPlatform = {
     "x86_64-linux" = {
       name = "linux_amd64";
-      hash = "sha256-v0KinrJHm1+n3oVElTvchoVXlnZR8mpO1rZPm59xCow=";
+      hash = "sha256-eWGc9QaqA7AfOJ+Mg+MU2LurKdtRNQ2VrfJZvRArHVo=";
     };
     "aarch64-linux" = {
       name = "linux_arm64";
-      hash = "sha256-qJHEhTwrRPlEVH5yoiPGCXjAeCXEPvPiSan7keJ3BoI=";
+      hash = "sha256-69DrP+qwwCZNa9NpYc5g1ne4IukP62imcW4gpXD/Jzg=";
     };
     "x86_64-darwin" = {
       name = "darwin_amd64";
-      hash = "sha256-n5BmKbIqfRKILDRhofjGezwjjFHyUSaXB9a3cGRx7FI=";
+      hash = "sha256-U0a1BcW7Y9vpYJLMOFcWzD8BBlGd88jknmtkdJjGNm0=";
     };
     "aarch64-darwin" = {
       name = "darwin_arm64";
-      hash = "sha256-V9DZT0LIyLbOZZ3FCLZ6StKEoPWayAxFCPyllfcREx0=";
+      hash = "sha256-h8JallK0IPR8BnqAZxzfSL/GYH8twT0Zim91rdw0wCY=";
     };
   };
   platform = systemToPlatform.${system} or throwSystem;
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "astro-cli";
-  version = "1.42.0";
+  version = "1.42.1";
 
   src = fetchurl {
     url = "https://github.com/astronomer/astro-cli/releases/download/v${finalAttrs.version}/astro_${finalAttrs.version}_${platform.name}.tar.gz";
